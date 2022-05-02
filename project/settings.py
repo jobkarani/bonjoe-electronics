6208b15184eae9f39ad3a483f3d63f192f7207b8
Job Karani <jobkarani084j@gmail.com>
2022-05-02 09:45:29 +0300

    sets up static

--- project/settings.py
+++ project/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join("assets"),
 ]
 
 # Default primary key field type
