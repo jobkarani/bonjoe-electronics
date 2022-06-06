5fdb0441b2c991d88c161ee6ae37f524e490be78
Job Karani <jobkarani084j@gmail.com>
2022-06-06 22:53:37 +0300

    implements pagination in shop page

--- app/views.py
+++ app/views.py
@@ -98,10 +98,10 @@ def index(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True).first(6)
+        products = Product.objects.filter(category=categories, is_available=True)[0:6]
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True).first(6)
+        products = Product.objects.all().filter(is_available=True)[0:6]
         product_count = products.count()
     context = {
         'products': products,
@@ -113,17 +113,16 @@ def index(request, category_slug=None):
 def shop(request, category_slug=None):
     categories = None
     products = None
-    # price_filter = Product.objects.filter(new_price__level__gte=0)
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 3)
+        paginator = Paginator(products, 9)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
-        paginator = Paginator(products, 3)
+        paginator = Paginator(products, 9)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
         product_count = products.count()
