48161afea012e492b41a3c5c0263f36843afbc52
Job Karani <jobkarani084j@gmail.com>
2022-06-07 09:27:37 +0300

    creates path for privacy policy

--- app/urls.py
+++ app/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('wishlist/', views.wishlist, name='wishlist'),
     path('add_wishlist/', views.add_wishlist, name='add_wishlist'),
+    path('privacypolicy/', views.privacypolicy, name='privacypolicy'),
     # path('userPayment/', views.userPayment, name='userPayment'),
 ]
