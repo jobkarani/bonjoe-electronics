1b054420b15e476a7454d3b5b063a3a796f39952
derrickmacharia <derrick.macharia@student.moringaschool.com>
2022-06-07 07:13:40 -0700

    fix card in home and shop page

--- app/views.py
+++ app/views.py
@@ -102,10 +102,10 @@ def index(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)[0:6]
+        products = Product.objects.filter(category=categories, is_available=True)[0:8]
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)[0:6]
+        products = Product.objects.all().filter(is_available=True)[0:8]
         product_count = products.count()
     context = {
         'products': products,
